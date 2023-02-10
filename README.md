d6da3220e39eee4993f9b2a43f0201f42e75ccdf
fago <fago@users.noreply.github.com>
2023-02-10 16:03:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# drunomics-lupus-decoupled-nuxt3-demo-3zbsqq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/drunomics-lupus-decoupled-nuxt3-demo-3zbsqq)
---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <MainNavigation />
+    <Messages />
+    <Breadcrumbs />
+    <component :is="renderCustomElements(page.content)" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { fetchPage, renderCustomElements } = useDrupalCe()
+const page = await fetchPage(useRoute().path, { query: useRoute().query })
+</script>
